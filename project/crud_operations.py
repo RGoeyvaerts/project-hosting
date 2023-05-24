bfa76507a8c86f15af3b0cd1eba0959210d1ea4f
JonasDeWeerdt <r0879844@students.thomasmore.be>
2023-05-24 14:46:34 +0200

    scuffed some things

--- project/crud_operations.py
+++ project/crud_operations.py
@@ -3,14 +3,8 @@ from sqlalchemy.orm import Session
 import auth
 import models
 import schemas
-#import subprocess
-#import os
-from kubernetes import client, config
-
-
-config.load_incluster_config()
-
-k8s_client = client.CoreV1Api()
+# import subprocess
+# import os
 
 
 def get_user(db: Session, user_id: int):
@@ -24,16 +18,6 @@ def get_user_by_email(db: Session, email: str):
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = auth.get_password_hash(user.password)
     db_user = models.User(email=user.email, hashed_password=hashed_password)
-    #home_dir = "/home/" + user.email
-    #subprocess.run(['sudo', 'useradd', '-m', user.email])
-    #subprocess.run(['sudo', 'chpasswd'], input=f"{user.email}:{user.password}", encoding='utf-8')
-    async def add_pod():
-        pod = client.V1Pod()
-        pod.api_version = "v1"
-        pod.kind = "Pod"
-        pod.metadata = client.V1ObjectMeta(name="test")
-        pod.spec = client.V1PodSpec(containers="{'name': 'my-container'', 'image': 'userpod'}")
-        response = k8s_client.create_namespaced_pod(namespace="default", body=pod)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -42,16 +26,15 @@ def create_user(db: Session, user: schemas.UserCreate):
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
+    # home_dir = "/home/" + user.email
+    # subprocess.run(['sudo', 'useradd', '-m', user.email])
+    # subprocess.run(['sudo', 'chpasswd'], input=f"{user.email}:{user.password}", encoding='utf-8')
 
 
 def set_password(db: Session, user: schemas.UserCreate):
     hashed_password = auth.get_password_hash(user.password)
     user_db = db.query(models.User).filter(
         models.User.email == user.email).first()
-    print(user_db)
-    if not user_db:
-        raise HTTPException(status_code=404, detail="User not found")
-
     user_db.hashed_password = hashed_password
     db.commit()
 
